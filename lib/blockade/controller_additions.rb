54bc2506451539060f8b2da5de9fdf3857d04248
Zamith <zamith.28@gmail.com>
2013-10-17 23:47:39 +0100

    Works, though very limited

--- lib/blockade/controller_additions.rb
+++ lib/blockade/controller_additions.rb
@@ -1,13 +1,38 @@
 module Blockade
+  class AccessDenied < StandardError; end
+
   module ControllerAdditions
     def self.included(base)
       base.extend ClassMethods
     end
 
+    def roles
+      raise NotImplementedError
+    end
+
+    def allowx(*args)
+      allowed = false
+      self.class.instance_variable_get(:@roles).each do |role, permissions|
+        if roles.public_send("#{role}?")
+          allowed = true
+          break
+        end
+      end
+      raise Blockade::AccessDenied unless allowed
+    end
+
     module ClassMethods
       def allow(*args)
-        self.before_filter :allow
+        @roles ||= {}
+        @roles[args.first] = []
+        self.before_filter :allowx
       end
     end
+  end
+end
+
+if defined? ActionController::Base
+  ActionController::Base.class_eval do
+    include Blockade::ControllerAdditions
   end
 end
